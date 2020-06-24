d3b3699c0a0b0928c4fca530b17a5743fb13a64e
Altonotch <info@altonotch.com>
2020-06-24 03:13:26 +0300

    Add ws with auth via token header
    Signed-off-by: Altonotch <info@altonotch.com>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 #[macro_use]
 extern crate log;
 
+use std::convert::Infallible;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
 use futures::stream::SplitSink;
 use futures::{SinkExt, StreamExt};
 use pretty_env_logger;
 use serde::{Deserialize, Serialize};
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 use structopt::StructOpt;
+use thiserror::Error;
 use tokio::time::Duration;
 use warp::filters::ws::{Message, WebSocket};
+use warp::http::StatusCode;
 use warp::Filter;
 
+const API_TOKEN: &str = "6smtr8ke3s7yq63f3zug9z3th";
+
+// command line arguments to modify the server,
+// for example to change the port of the server
 #[derive(StructOpt, Debug)]
 #[structopt(name = "websocket-server")]
 struct Opts {
@@ -19,17 +27,36 @@ struct Opts {
     port: u16,
 }
 
+// example request structure
 #[derive(Deserialize, Debug)]
 struct WsRequest {
     kind: String,
     message: String,
 }
 
-#[derive(Serialize)]
+// example result structure
+#[derive(Serialize, Debug)]
 struct WsResult {
     status: String,
     response: String,
 }
+
+// example error response
+#[derive(Serialize, Debug)]
+struct ApiErrorResult {
+    detail: String,
+}
+
+// errors thrown by handlers and custom filters,
+// such as `ensure_authentication` filter
+#[derive(Error, Debug)]
+enum ApiErrors {
+    #[error("user not authorized")]
+    NotAuthorized(String),
+}
+
+// ensure that warp`s Reject recognizes `ApiErrors`
+impl warp::reject::Reject for ApiErrors {}
 
 #[tokio::main]
 async fn main() {
@@ -39,20 +66,82 @@ async fn main() {
     info!("initializing server on port: {}", opt.port);
 
     let health_check = warp::path("health-check").map(|| format!("Server OK"));
+
     let ws = warp::path("ws").and(warp::ws()).map(|ws: warp::ws::Ws| {
         info!("upgrading connection to websocket");
         ws.on_upgrade(handle_ws_client)
     });
 
-    let routes = health_check.or(ws).with(warp::cors().allow_any_origin());
+    let ws_auth = warp::path("ws-private")
+        .and(ensure_authentication().await)
+        .and(warp::ws())
+        .map(|user: String, ws: warp::ws::Ws| {
+            info!("upgrading authorized connection to websocket: {}", user);
+            ws.on_upgrade(handle_ws_client)
+        });
+
+    let routes = health_check
+        .or(ws)
+        .or(ws_auth)
+        .with(warp::cors().allow_any_origin())
+        .recover(handle_rejection);
 
     warp::serve(routes)
-        .run(SocketAddr::new(
-            IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
-            opt.port,
-        ))
+        .run(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), opt.port))
         .await;
     info!("server is running");
+}
+
+// generic errors handler for all api errors
+// ensures unified error structure
+async fn handle_rejection(err: warp::reject::Rejection) -> std::result::Result<impl warp::reply::Reply, Infallible> {
+    let code;
+    let message;
+
+    if err.is_not_found() {
+        code = StatusCode::NOT_FOUND;
+        message = "Not found";
+    } else if let Some(_) = err.find::<warp::filters::body::BodyDeserializeError>() {
+        code = StatusCode::BAD_REQUEST;
+        message = "Invalid Body";
+    } else if let Some(e) = err.find::<ApiErrors>() {
+        match e {
+            ApiErrors::NotAuthorized(_error_message) => {
+                code = StatusCode::UNAUTHORIZED;
+                message = "Action not authorized";
+            }
+        }
+    } else if let Some(_) = err.find::<warp::reject::MethodNotAllowed>() {
+        code = StatusCode::METHOD_NOT_ALLOWED;
+        message = "Method not allowed";
+    } else {
+        // We should have expected this... Just log and say its a 500
+        error!("unhandled rejection: {:?}", err);
+        code = StatusCode::INTERNAL_SERVER_ERROR;
+        message = "Internal server error";
+    }
+
+    let json = warp::reply::json(&ApiErrorResult { detail: message.into() });
+
+    Ok(warp::reply::with_status(json, code))
+}
+
+// middleware that looks for authorization header and validates it
+async fn ensure_authentication() -> impl Filter<Extract = (String,), Error = warp::reject::Rejection> + Clone {
+    warp::header::optional::<String>("Authorization").and_then(|auth_header: Option<String>| async move {
+        info!("doing dummy validation of authorization header");
+        if let Some(header) = auth_header {
+            info!("got auth header, verifying: {}", header);
+            let parts: Vec<&str> = header.split(" ").collect();
+            if parts.len() == 2 && parts[0] == "Token" && parts[1] == API_TOKEN {
+                return Ok("Existing user".to_string());
+            }
+        }
+
+        Err(warp::reject::custom(ApiErrors::NotAuthorized(
+            "not authorized".to_string(),
+        )))
+    })
 }
 
 async fn handle_ws_client(websocket: warp::ws::WebSocket) {
@@ -80,7 +169,7 @@ async fn handle_websocket_message(message: Message, sender: &mut SplitSink<WebSo
     let msg = if let Ok(s) = message.to_str() {
         s
     } else {
-        info!("ping-ping");
+        info!("ping-pong");
         return;
     };
 
